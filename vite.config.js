ae7c82bdfac220d95e5cbe17e445a4a5eefd6b17
Robert <robetdlc@gmail.com>
2026-03-14 17:31:56 -0500

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/Janery/', // nombre del repo para GitHub Pages
-  plugins: [react()],
-})
